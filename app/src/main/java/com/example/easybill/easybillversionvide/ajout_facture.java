158410f0b0da34f0b97e18c3a5175179c167cb3e
louis2580 <louis0710@live.fr>
2017-12-01 11:42:04 -0500

    limite de taille prix et lieu, cache le clavier une fois sur le calendrier

--- app/src/main/java/com/example/easybill/easybillversionvide/ajout_facture.java
+++ app/src/main/java/com/example/easybill/easybillversionvide/ajout_facture.java
@@ -15,6 +15,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -137,6 +138,9 @@ public class ajout_facture extends AppCompatActivity {
                     new DatePickerDialog(ajout_facture.this, date, calendar
                             .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
                             calendar.get(Calendar.DAY_OF_MONTH)).show();
+
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
 
                 }
             }
